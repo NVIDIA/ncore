7330bdd373b5fd647adb41539ff7758246007310
Janick Martinez Esturo <janickm@nvidia.com>
2022-07-06 13:41:53 +0200

    - add 'debugpy' support for convert_raw_data.py

--- scripts/convert_raw_data.py
+++ scripts/convert_raw_data.py
@@ -2,11 +2,15 @@
 # Copyright (c) 2022 NVIDIA CORPORATION.  All rights reserved.
 
 import click
+import debugpy
+import logging
 
 from src.dataset_converter import DataConverter
 from src.dataset_converter.waymo_open import WaymoConverter
 from src.dataset_converter.nvidia_deepmap import NvidiaDeepMapConverter
 from src.dataset_converter.nvidia_maglev import NvidiaMaglevConverter
+
+logger = logging.getLogger(__name__)
 
 @click.group()
 @click.option('--root-dir', type=str, help="Path to the raw data sequences", required=True)
@@ -15,6 +19,9 @@ from src.dataset_converter.nvidia_maglev import NvidiaMaglevConverter
 @click.option('--semantic-seg', is_flag=True, default=False, help="Infer the semantic segmention for all camera images")
 @click.option('--instance-seg', is_flag=True, default=False, help="Infer the instance segmention for all camera images")
 @click.option('--surface-rec', is_flag=True, default=False, help="Reconstruct the static background mesh")
+@click.option("--debug", is_flag=True, default=False, help="Enables a debugpy client to connect to the port specified by --debug-port")
+@click.option("--debug-wait-for-client", is_flag=True, default=False, help="Enables a debugpy client to connect to the port specified by --debug-port and waits for a client to connect on start-up")
+@click.option("--debug-port", default=5678, type=int, help="Configure the TCP port to use for debugging")
 @click.version_option('0.1')
 @click.pass_context
 def cli(ctx, *_, **kwargs):
@@ -35,6 +42,19 @@ def cli(ctx, *_, **kwargs):
     # this point onwards other commands can refer to it by using the
     # @click.pass_context decorator.
     ctx.obj = DataConverter.Config(kwargs)
+
+    # Conditionally enable debugging
+    if ctx.obj.debug or ctx.obj.debug_wait_for_client:
+        # Note: enabling debug impacts the performance of system calls and Python code execution.
+        logger.info("Listening for incoming debug connection on port {}".format(
+            ctx.obj.debug_port))
+        debugpy.listen(("0.0.0.0", ctx.obj.debug_port))
+
+        if ctx.obj.debug_wait_for_client:
+            logger.info("Waiting for incoming debug connection on port {}".format(
+                ctx.obj.debug_port))
+            # Block until a client connects
+            debugpy.wait_for_client()
 
 
 @cli.command()
