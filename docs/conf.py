188b964185ca401a723330ecf29b729dc81edbf6
Zan Gojcic <zgojcic@nvidia.com>
2022-09-26 11:11:43 +0200

    Sphinx documentation of the data formats

---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DSAI'
+copyright = '2022, NVIDIA'
+author = 'NVIDIA - Toronto AI Lab'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.todo',
+]
+
+todo_include_todos = True
+
+intersphinx_mapping = {
+    'python': ("https://docs.python.org/3", None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'PyTorch': ('https://pytorch.org/docs/master/', None),
+}
+
+master_doc = 'index'
+
+templates_path = ['_templates']
+exclude_patterns = ['_build']
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+import sphinx_rtd_theme
+
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {
+    'collapse_navigation': False
+}
---
+++ docs/index.rst
@@ -0,0 +1,31 @@
+.. DSAI documentation master file, created by
+   sphinx-quickstart on Fri Sep 23 09:31:43 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DSAI Library Documentation!
+
+DSAI Library provides the tools developed in the Nvidia Toronto AI Lab for data-driven simulation in autonomous driving. 
+
+Take a look at the Installation guide, :ref:`conventions` and Tutorials to get started. 
+
+
+================================
+
+.. toctree::
+   :maxdepth: 1
+   :numbered:
+   :caption: Introduction:
+
+   notes/conventions
+
+
+.. note:: 
+   This project is under active development.
+   
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
