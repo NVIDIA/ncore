7f5e05347ba29ffc32b25cb62becc1c90ee7dc2f
Zan Gojcic <zgojcic@nvidia.com>
2022-10-11 11:47:00 +0200

    replace stop-frame with end-frame to be consistent

--- scripts/dsai_extract_segmentation.py
+++ scripts/dsai_extract_segmentation.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 @click.option('--semantic-seg', is_flag=True, default=False, help="Perform semantic segmentation")
 @click.option('--instance-seg', is_flag=True, default=False, help="Perform instance segmentation") 
 @click.option('--start-frame', type=int, help="The starting frame of the sequence", default=0)
-@click.option('--stop-frame', type=int, help="The end frame of the sequence", default=-1)
+@click.option('--end-frame', type=int, help="The end frame of the sequence", default=-1)
 @click.option("--index-digits", type=int, help="The number of integer digits to pad counters in output filenames to", default=6)
 @click.version_option('0.1')
 @click.pass_context
@@ -30,7 +30,7 @@ def cli(ctx, *_, **kwargs):
 def waymo(ctx, *_, **kwargs):
 
     assert os.path.exists(ctx.obj['root_dir']), "Provided root_dir path doesn't exist."
-    assert (ctx.obj['start_frame'] < ctx.obj['stop_frame'] or ctx.obj['stop_frame'] == -1), "stop frame must be larger than start frame"
+    assert (ctx.obj['start_frame'] < ctx.obj['end_frame'] or ctx.obj['end_frame'] == -1), "stop frame must be larger than start frame"
 
     # Check if the selected cameras are valid
     if len(ctx.obj['cameras']) == 1 and ctx.obj['cameras'][0] == -1: 
@@ -44,31 +44,31 @@ def waymo(ctx, *_, **kwargs):
         imgs = sorted(glob.glob(os.path.join(ctx.obj['root_dir'], f'images/image_{str(cam).zfill(2)}', f"{'?'*ctx.obj['index_digits']}.jpeg")))
 
         start_frame = ctx.obj['start_frame']
-        stop_frame = ctx.obj['stop_frame']
+        end_frame = ctx.obj['end_frame']
         if ctx.obj['start_frame']  > len(imgs):
             logging.warning(f"The start frame is larger than the number of images. All images will be processed")
             start_frame = 0
-            stop_frame = len(imgs)
+            end_frame = len(imgs)
 
-        elif ctx.obj['stop_frame'] != -1 and ctx.obj['stop_frame'] > len(imgs):
+        elif ctx.obj['end_frame'] != -1 and ctx.obj['end_frame'] > len(imgs):
             logging.warning(f"The stop frame is larger than the number of images. All frames larger than start frame will be processed")
-            stop_frame = len(imgs)
+            end_frame = len(imgs)
         
-        elif ctx.obj['stop_frame'] == -1:
-            stop_frame = len(imgs)
+        elif ctx.obj['end_frame'] == -1:
+            end_frame = len(imgs)
             
         if ctx.obj['semantic_seg']:
-            run_semantic_segmentation(imgs[start_frame:stop_frame + 1], ctx.obj['index_digits'])
+            run_semantic_segmentation(imgs[start_frame:end_frame + 1], ctx.obj['index_digits'])
 
         if ctx.obj['instance_seg']:
-            run_instance_segmentation(imgs[start_frame:stop_frame + 1])
+            run_instance_segmentation(imgs[start_frame:end_frame + 1])
 
 @cli.command()
 @click.pass_context
 def nvidia(ctx, *_, **kwargs):
 
     assert os.path.exists(ctx.obj['root_dir']), "Provided root_dir path doesn't exist."
-    assert (ctx.obj['start_frame'] < ctx.obj['stop_frame'] or ctx.obj['stop_frame'] == -1), "stop frame must be larger than start frame"
+    assert (ctx.obj['start_frame'] < ctx.obj['end_frame'] or ctx.obj['end_frame'] == -1), "stop frame must be larger than start frame"
 
     # Check if the selected cameras are valid
     if len(ctx.obj['cameras']) == 1 and ctx.obj['cameras'][0] == -1: 
@@ -81,24 +81,24 @@ def nvidia(ctx, *_, **kwargs):
         imgs = sorted(glob.glob(os.path.join(ctx.obj['root_dir'], f'images/image_{str(cam).zfill(2)}', f"{'?'*ctx.obj['index_digits']}.jpeg")))
 
         start_frame = ctx.obj['start_frame']
-        stop_frame = ctx.obj['stop_frame']
+        end_frame = ctx.obj['end_frame']
         if ctx.obj['start_frame']  > len(imgs):
             logging.warning(f"The start frame is larger than the number of images. All images will be processed")
             start_frame = 0
-            stop_frame = len(imgs)
+            end_frame = len(imgs)
 
-        elif ctx.obj['stop_frame'] != -1 and ctx.obj['stop_frame'] > len(imgs):
+        elif ctx.obj['end_frame'] != -1 and ctx.obj['end_frame'] > len(imgs):
             logging.warning(f"The stop frame is larger than the number of images. All frames larger than start frame will be processed")
-            stop_frame = len(imgs)
+            end_frame = len(imgs)
          
-        elif ctx.obj['stop_frame'] == -1:
-            stop_frame = len(imgs)
+        elif ctx.obj['end_frame'] == -1:
+            end_frame = len(imgs)
 
         if ctx.obj['semantic_seg']:
-            run_semantic_segmentation(imgs[start_frame:stop_frame + 1], ctx.obj['index_digits'])
+            run_semantic_segmentation(imgs[start_frame:end_frame + 1], ctx.obj['index_digits'])
 
         if ctx.obj['instance_seg']:
-            run_instance_segmentation(imgs[start_frame:stop_frame + 1])
+            run_instance_segmentation(imgs[start_frame:end_frame + 1])
 
 if __name__ == "__main__":
     cli(show_default=True)
