4f53b6af490549f9eef915d039c54bfed5b0b71d
Janick Martinez Esturo <janickm@nvidia.com>
2022-10-12 15:28:14 +0200

    - add LANCZOS-based image resizing

--- scripts/experimental/dsai_downsample_camera.py
+++ scripts/experimental/dsai_downsample_camera.py
@@ -8,12 +8,13 @@ import pickle
 
 from typing import Tuple
 
+from PIL import Image
 import numpy as np
 
 from src.py.common.nvidia_utils import (compute_ftheta_parameters, compute_fw_polynomial)
 
 
-def downsample_ftheta(intrinsic_array, downsample) -> Tuple[np.array, Tuple[int, int]]:
+def downsample_ftheta(intrinsic_array, inverse_downsample_factor) -> Tuple[np.array, Tuple[int, int]]:
     ''' Decode relevant / non-'derived' camera model parameters (backward poly only, forward-poly + ray limits are derived)
         and perform camera model downsampling '''
 
@@ -36,20 +37,20 @@ def downsample_ftheta(intrinsic_array, downsample) -> Tuple[np.array, Tuple[int,
     ## Downsample resolution
 
     # check if requested downsampling is supported by model's resolution (not resulting in fractional resolutions)
-    assert img_width % downsample == 0 and img_height % downsample == 0, 'incompatible downsample factor for image resolution'
+    assert img_width % inverse_downsample_factor == 0 and img_height % inverse_downsample_factor == 0, 'incompatible downsample factor for image resolution'
 
-    img_width_downsampled = img_width // downsample
-    img_height_downsampled = img_height // downsample
+    img_width_downsampled = img_width // inverse_downsample_factor
+    img_height_downsampled = img_height // inverse_downsample_factor
 
-    ## Downsample transform principal point location in downsampled image
-    cxcy_downsampled = cxcy / downsample
+    ## Downsample principal point location by transforming it in the downsampled image
+    cxcy_downsampled = cxcy / inverse_downsample_factor
 
     ## Downsample polynomial by substituting the input pixel domain transformation (backwards polynomial is a pixel-distance to angle map, so the domain needs to be scaled)
-    downsample_pixel_map = np.polynomial.Polynomial([0.0, downsample])
+    downsample_pixel_map = np.polynomial.Polynomial([0.0, inverse_downsample_factor])
     bw_poly_downsampled = bw_poly(downsample_pixel_map)
 
     # sanity check that the new transformation is a correctly downscaled version of the original polynomial
-    assert bw_poly(1.0) == bw_poly_downsampled(1.0 / downsample), 'computed invalid polynomial map'
+    assert bw_poly(1.0) == bw_poly_downsampled(1.0 / inverse_downsample_factor), 'computed invalid polynomial map'
 
     ## Update intrinsics and estimate derived parameters + prepare outputs in V1 format
     intrinsic_array_downsampled = np.hstack(
@@ -68,15 +69,23 @@ def downsample_ftheta(intrinsic_array, downsample) -> Tuple[np.array, Tuple[int,
 @click.command()
 @click.option('--root-dir', type=str, help='Path to the preprocessed DSAI sequence to update', required=True)
 @click.option('--cam-id', type=str, help='Camera to be used', default='00')
-@click.option('--downsample', type=click.IntRange(min=2, max_open=True), default=2, help='Inverse downsample factor')
-def dsai_downsample_camera(root_dir: str, cam_id: int, downsample: int):
+@click.option('--downsample-factor',
+              'inverse_downsample_factor',
+              type=click.IntRange(min=2, max_open=True),
+              default=2,
+              help='*Inverse* downsample factor')
+@click.option('--downsample-images',
+              is_flag=True,
+              default=False,
+              help='Downsample image data in addition to camera model')
+def dsai_downsample_camera(root_dir: str, cam_id: int, inverse_downsample_factor: int, downsample_images: bool):
     ''' Downsamples FTheta Camera Model '''
 
     # Initialize the logger
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    logger.info(f'Downsampling camera {cam_id} with downsample_factor = {1.0 / downsample} <{root_dir}>')
+    logger.info(f'Downsampling camera {cam_id} with downsample_factor = {1.0 / inverse_downsample_factor} <{root_dir}>')
 
     camera_meta_paths = sorted(glob.glob(os.path.join(root_dir, f'images/image_{cam_id}/*.pkl')))
 
@@ -87,7 +96,8 @@ def dsai_downsample_camera(root_dir: str, cam_id: int, downsample: int):
         assert meta['camera_model'] == 'f_theta', 'currently only support downsampling of ftheta cameras'
 
         intrinsic_array_downsampled, (img_width_downsampled,
-                                      img_height_downsampled) = downsample_ftheta(meta['intrinsic'], downsample)
+                                      img_height_downsampled) = downsample_ftheta(meta['intrinsic'],
+                                                                                  inverse_downsample_factor)
 
     # Update all intrinsics in camera-associated meta files
     for camera_meta_path in camera_meta_paths:
@@ -101,6 +111,18 @@ def dsai_downsample_camera(root_dir: str, cam_id: int, downsample: int):
 
         with open(camera_meta_path, 'wb') as f:
             pickle.dump(meta, f)
+
+    if downsample_images:
+        for image_path in set(
+                glob.glob(os.path.join(root_dir, f'images/image_{cam_id}/*.jpg')) +
+                glob.glob(os.path.join(root_dir, f'images/image_{cam_id}/*.jpeg')) +
+                glob.glob(os.path.join(root_dir, f'images/image_{cam_id}/*.png'))):
+            img = Image.open(image_path)
+            img_width, img_height = img.size[0], img.size[1]
+            assert img_width % inverse_downsample_factor == 0 and img_height % inverse_downsample_factor == 0, 'incompatible downsample factor for image resolution'
+            img = img.resize((img_width // inverse_downsample_factor, img_height // inverse_downsample_factor),
+                             Image.Resampling.LANCZOS)
+            img.save(image_path)
 
 
 if __name__ == "__main__":
