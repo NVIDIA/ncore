1d46efe87fcc7b5caa6822733dcb13bb21a1565a
Janick Martinez Esturo <janickm@nvidia.com>
2022-12-08 14:42:52 +0100

    - add public dsai.data.v3

--- src/dsai/data/v2/__init__.py
+++ src/dsai/data/v2/__init__.py
@@ -1,5 +1,4 @@
 # Copyright (c) 2022 NVIDIA CORPORATION.  All rights reserved.
 ''' Package exposing methods related to DSAI's V2 data interaction APIs '''
 
-from src.dsai_internal.data.data2 import (CameraSensor, DataLoader, DataWriter, LidarSensor, PointCloudSensor,
-                                          RadarSensor, Sensor)
+from src.dsai_internal.data.data2 import (CameraSensor, DataLoader, LidarSensor, PointCloudSensor, RadarSensor, Sensor)
