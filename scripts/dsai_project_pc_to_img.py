c828e33d9a3ee70823d86d79fc0a0184dbbfec90
Janick Martinez Esturo <janickm@nvidia.com>
2022-12-09 14:23:28 +0100

    - adapt dsai_project_pc_to_img to V3

--- scripts/dsai_project_pc_to_img.py
+++ scripts/dsai_project_pc_to_img.py
@@ -6,15 +6,18 @@ import tqdm
 
 import numpy as np
 
-from src.dsai_internal.data.data2 import DataLoader, PointCloudSensor, CameraSensor 
-from src.dsai_internal.data import types 
+from src.dsai_internal.data.data3 import ShardDataLoader, PointCloudSensor, CameraSensor
+from src.dsai_internal.data import types
 from src.dsai_internal.common.transformations import transform_point_cloud
 from src.dsai_internal.av_utils import rollingShutterProjection
 from src.dsai_internal.common.visualization import plot_points_on_image
 from src.dsai_internal.sensors.camera import CameraModel
 
 @click.command()
-@click.option('--root-dir', type=str, help='Path to the preprocessed sequence', required=True)
+@click.option('--shard-file-pattern',
+              type=str,
+              help='Data shard pattern to load (supports range expansion)',
+              required=True)
 @click.option('--sensor-id', type=str, help='Sensor whose point cloud will be projected', required=True)
 @click.option('--camera-id', type=str, help='Sensor to export ply files for', required=True)
 @click.option('--start-frame',
@@ -28,22 +31,23 @@ from src.dsai_internal.sensors.camera import CameraModel
 @click.option('--step-frame',
               type=click.IntRange(min=1, max_open=True),
               help='Step used to downsample the number of frames',
-              default=1)    
+              default=1)
 @click.option('--device',
               type=click.Choice(['gpu', 'cpu', 'both']),
               help='Device used for the computation. If gpu - projection will be done in pytorch on a gpu.',
               default='cpu')
 
 
-def dsai_project_pc_to_img(root_dir: str, sensor_id: str, camera_id: str, start_frame: int, end_frame: int,
-                    step_frame: int, device: str):
+def dsai_project_pc_to_img(shard_file_pattern: str, sensor_id: str, camera_id: str, start_frame: int, end_frame: int,
+                           step_frame: int, device: str):
     ''' Projects the point cloud to the camera image, comparing projection w. and w/o rolling shutter compensation  '''
 
     # Initialize the logger
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    loader = DataLoader(root_dir)
+    shards = ShardDataLoader.evaluate_shard_file_pattern(shard_file_pattern)
+    loader = ShardDataLoader(shards)
     pc_sensor = loader.get_sensor(sensor_id)
     assert isinstance(pc_sensor, PointCloudSensor), 'only point-cloud sensors are supported as source sensor'
     cam_sensor = loader.get_sensor(camera_id)
@@ -57,18 +61,18 @@ def dsai_project_pc_to_img(root_dir: str, sensor_id: str, camera_id: str, start_
         # Get the camera timestamp and find the closes lidar frame
         cam_timestamp = cam_sensor.get_frame_timestamp_us(frame_index)
         pc_frame_index = pc_sensor.get_closest_frame_index(cam_timestamp)
-        
+
         # Load the camera image and the point cloud
         img_frame = cam_sensor.get_frame_image_array(frame_index)
         pc = pc_sensor.get_frame_data(pc_frame_index, 'xyz_e')
 
         # Transform the point cloud to the world coordinate frame
         pc = transform_point_cloud(pc, pc_sensor.get_frame_T_sensor_world(pc_frame_index))
-        
+
         T_world_sensor_start = cam_sensor.get_frame_T_world_sensor(frame_index, types.FrameTimepoint.START)
         T_world_sensor_end = cam_sensor.get_frame_T_world_sensor(frame_index, types.FrameTimepoint.END)
         T_world_sensor = np.stack([T_world_sensor_start, T_world_sensor_end])
-        
+
         # Initialize the camera model
         cam_model_params = cam_sensor.get_camera_model_parameters()
         cam_model = CameraModel.from_parameters(cam_model_params)
@@ -84,7 +88,7 @@ def dsai_project_pc_to_img(root_dir: str, sensor_id: str, camera_id: str, start_
             transformed_points = transform_point_cloud(pc[valid_idx_torch,None,:], trans_matrices_torch).squeeze(1)
             dist_rs = np.linalg.norm(transformed_points, axis=1, keepdims=True)
 
-            plot_points_on_image(np.concatenate((pixel_coords_torch[:,:2], dist_rs),axis=1), img_frame, 
+            plot_points_on_image(np.concatenate((pixel_coords_torch[:,:2], dist_rs),axis=1), img_frame,
                                             "Projection with rolling shutter (torch GPU implementation)", point_size=4.0)
         if device in ['cpu', 'both']:
             logger.info(f"Starting the projection with a c++ CPU implementation.")
@@ -95,7 +99,7 @@ def dsai_project_pc_to_img(root_dir: str, sensor_id: str, camera_id: str, start_
             transformed_points = transform_point_cloud(pc[valid_idx_rs,None,:], trans_matrices_rs).squeeze(1)
             dist_rs = np.linalg.norm(transformed_points, axis=1, keepdims=True)
 
-            plot_points_on_image(np.concatenate((pixel_coords_rs[:,:2], dist_rs),axis=1), img_frame, 
+            plot_points_on_image(np.concatenate((pixel_coords_rs[:,:2], dist_rs),axis=1), img_frame,
                                                 "Projection with rolling shutter (c++ implementation)", point_size=4.0)
 
 
