14d974f6cfeda69ac1df4e9d9af5759205d9ebb5
Janick Martinez Esturo <janickm@nvidia.com>
2022-12-20 09:47:52 +0100

    - tweak sample number to generate less shards + speed limit

--- scripts/experimental/sd-data/sd_shards.py
+++ scripts/experimental/sd-data/sd_shards.py
@@ -15,8 +15,8 @@ logger = logging.getLogger(__name__)
 @click.option('--shard-id', type=click.IntRange(min=0, max_open=True), default=0, help="Shard id in [0,N-1] controlling uniform dataset subset processing")
 @click.option('--shard-count', type=click.IntRange(min=1, max_open=True), default=1, help="Total number of shards N to performing full dataset processing")
 @click.option("--wds-shard-maxsize-mib", type=click.IntRange(min=1, max_open=True), default=4096, help="Maximum size per webdataset shard in MiB")
-@click.option("--wds-shard-maxsamples", type=click.IntRange(min=1, max_open=True), default=1000, help="Target number of samples per webdataset shard")
-@click.option("--min-speed-km-h", type=click.FloatRange(min=0.0, max_open=True), default=None, help="If provided, the minimum required speed to filter out low-motion data (kilometer/hour)")
+@click.option("--wds-shard-maxsamples", type=click.IntRange(min=1, max_open=True), default=100000, help="Target number of samples per webdataset shard")
+@click.option("--min-speed-km-h", type=click.FloatRange(min=0.0, max_open=True), default=10, help="If provided, the minimum required speed to filter out low-motion data (kilometer/hour)")
 @click.pass_context
 def sd_shards(ctx, *_, **kwargs):
     """NVIDIA-specific data conversion (Stable-Diffusion data, based on Maglev data extraction)"""
