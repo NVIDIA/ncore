- fix principal-axis Jacobians of FTheta
Introduce an "epsilon" trick to correctly compute Jacobians around
the differentiable part of the domain close to the
principal point. Extend unit tests to verify correctness

ncore/impl/sensors/camera.py
@@ -962,11 +962,10 @@ class FThetaCameraModel(CameraModel):
 
         ray_norm = self.__nummerically_stable_norm(cam_rays)
 
-        # Only process rays with non-vanishing norms
-        valid_norm = ray_norm[:, 0] > 0.0
-        valid_norm_idx = torch.where(valid_norm)[0]
-
-        alphas_full = torch.atan2(ray_norm[valid_norm_idx], cam_rays[valid_norm_idx, 2:])
+        # Make sure norm is non-vanishing (norm vanishes for points along the principal-axis)
+        ray_norm[ray_norm[:, 0] <= 0.0] = torch.finfo(self.dtype).eps
+        
+        alphas_full = torch.atan2(ray_norm[:], cam_rays[:, 2:])
 
         # Limit angles to max_angle to prevent projected points to leave valid cone around max_angle.
         # In particular for omnidirectional cameras, this prevents points outside the FOV to be
@@ -980,32 +979,28 @@ class FThetaCameraModel(CameraModel):
         delta = self.__eval_poly_inverse_horner_newton(self.bw_poly, self.dbw_poly, self.fw_poly,
                                                        self.newton_iterations, alphas)
 
-        scale = torch.ones_like(ray_norm)
-        scale[valid_norm_idx] = delta / ray_norm[valid_norm_idx]
+        scale = delta / ray_norm
         image_points = scale * cam_rays[:, :2] + self.principal_point[None, :]
 
         # Extract valid image points
-        valid_x = torch.logical_and(0.0 <= image_points[valid_norm_idx, 0], image_points[valid_norm_idx, 0] < self.resolution[0])
-        valid_y = torch.logical_and(0.0 <= image_points[valid_norm_idx, 1], image_points[valid_norm_idx, 1] < self.resolution[1])
+        valid_x = torch.logical_and(0.0 <= image_points[:, 0], image_points[:, 0] < self.resolution[0])
+        valid_y = torch.logical_and(0.0 <= image_points[:, 1], image_points[:, 1] < self.resolution[1])
         valid_delta = alphas[:, 0] < self.max_angle  # explicitly check for strictly smaller angles to classify FOV-clamped points as invalid
-        valid = valid_norm
-        valid[valid_norm_idx[~(valid_x & valid_y & valid_delta)]] = False
+        valid = valid_x & valid_y & valid_delta
 
         jacobians: Optional[torch.Tensor] = None
         if return_jacobians:
             # Evaluate Jacobians of valid points by gradients of both output dimensions
-            jacobians = torch.zeros((len(cam_rays), 2, 3), dtype=self.dtype, device=self.device)
-            jacobians[~valid_norm] = torch.tensor([[1, 0, 0], [0, 1, 0]]).to(
-                cam_rays)  # Jacobian for vanishing norm vectors (e.g., principal axis)
+            jacobians = torch.empty((len(cam_rays), 2, 3), dtype=self.dtype, device=self.device)
 
-            initial_gradient = torch.ones((len(valid_norm_idx), ), dtype=self.dtype, device=self.device)
-            image_points[valid_norm_idx, 0].backward(gradient=initial_gradient, retain_graph=True)
-            jacobians[valid_norm_idx, 0] = cam_rays.grad[valid_norm_idx, :]
+            initial_gradient = torch.ones((len(cam_rays), ), dtype=self.dtype, device=self.device)
+            image_points[:, 0].backward(gradient=initial_gradient, retain_graph=True)
+            jacobians[:, 0] = cam_rays.grad
 
             cam_rays.grad.zero_()
 
-            image_points[valid_norm_idx, 1].backward(gradient=initial_gradient)
-            jacobians[valid_norm_idx, 1] = cam_rays.grad[valid_norm_idx, :]
+            image_points[:, 1].backward(gradient=initial_gradient)
+            jacobians[:, 1] = cam_rays.grad
 
             # Cleanup for other backprop users
             cam_rays.grad.zero_()

ncore/impl/sensors/camera_test.py
@@ -701,11 +701,15 @@ class TestJacobian(CommonTestCase):
             def projection_wrapper(x):
                 return cam_model.camera_rays_to_image_points(x[None, :]).image_points.squeeze()
 
-            rays3d = cam_model.image_points_to_camera_rays(torch.Tensor([[20, 40], [11, 12], [15, 20],
-                                                                         [500, 500]]))  # valid rays
-            rays3d = torch.cat([rays3d,
-                                torch.Tensor([[1, 2, -5],
-                                              [0, 0, 0]]).to(rays3d)])  # add some "invalid" rays (behind camera / zero)
+            valid_rays3d = cam_model.image_points_to_camera_rays(
+                torch.Tensor([[20, 40], [11, 12], [15, 20], [500, 500]]))  # valid rays
+            principal_direction_rays3d = torch.Tensor([[0, 0, 1], 
+                                                       [0, 0, 5],
+                                                       [0, 0, 0.1]]).to(valid_rays3d)  # rays along the principal direction
+            invalid_rays3d = torch.Tensor([[1, 2, -5], [1, 2, 0], [0, 0, 0]]).to(
+                valid_rays3d
+            )  # some "invalid" rays (behind camera / on the center of projection plane but ouf of FOV / zero)
+            rays3d = torch.cat([valid_rays3d, principal_direction_rays3d, invalid_rays3d])
 
             # evaluate projection with jacobians
             proj = cam_model.camera_rays_to_image_points(rays3d, return_jacobians=True)
@@ -720,8 +724,8 @@ class TestJacobian(CommonTestCase):
                 np.testing.assert_array_almost_equal(Jref.cpu().numpy(), proj.jacobians[i].cpu().numpy())
 
                 self.assertTrue(
-                    proj.valid_flag[i] if i < 4 else
-                    not proj.valid_flag[i])  # First four rays should be flagged as valid, others should be invalid
+                    proj.valid_flag[i] if i < len(rays3d) - len(invalid_rays3d) else
+                    not proj.valid_flag[i])  # First rays should be flagged as valid, others should be invalid
 
 if __name__ == '__main__':
     unittest.main()