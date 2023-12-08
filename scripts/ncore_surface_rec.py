8df1f924a2d392e2c7afef4619beb40c3a8ba8c8
Janick Martinez Esturo <janickm@nvidia.com>
2023-12-08 11:22:47 +0100

    [surface-rec] Workaround PCU serialization issue
    MAJOR
    - `pcu.save_mesh_*()` has issues serializing meshes with
      large amounts of faces (resulting in partially invalid .ply files).
    
      Temporarily workaround by switching to trimesh-based
      serialization for now and revert back to PCU one the issues
      are fixed in a new release
    
    MINOR
    - remove unreferenced vertices before serialization (reduces file-size)
    - don't try to load color / normal from PoissonReconstruction outputs,
      as only geometry in generated by the tool

--- scripts/ncore_surface_rec.py
+++ scripts/ncore_surface_rec.py
@@ -11,6 +11,7 @@ from typing import Optional
 import click
 import numpy as np
 import point_cloud_utils as pcu
+import trimesh
 
 from ncore.impl.common.transformations import transform_point_cloud
 from ncore.impl.data.data3 import ShardDataLoader, PointCloudSensor
@@ -97,15 +98,22 @@ def ncore_surface_rec(
         )
 
         logger.info("Trimming the reconstructed mesh")
-        v, f, n, c = pcu.load_mesh_vfnc(temp_rp.name)
+        v, f = pcu.load_mesh_vf(temp_rp.name)
 
     nn_dist, _ = pcu.k_nearest_neighbors(v.astype(np.float32), pf.astype(np.float32), k=2)
     nn_dist = nn_dist[:, 1]
     f_mask = np.stack([nn_dist[f[:, i]] < trim_distance for i in range(f.shape[1])], axis=-1)
     f_mask = np.all(f_mask, axis=-1)
 
+    v_clean, f_clean, _, _ = pcu.remove_unreferenced_mesh_vertices(v, f[f_mask])
+
     logger.info("Saving the reconstructed mesh and cleaning up the temp files.")
-    pcu.save_mesh_vfnc(str(output_path / f"{output_filename}.ply"), v, f[f_mask], n, c)
+
+    # Note: we are serializing using 'trimesh' as PCU currently has an issue
+    # serializing correct ply files with large amounts of faces
+    trimesh.Trimesh(v_clean, f_clean, process=False).export(
+        str(output_path / f"{output_filename}.ply"), file_type="ply"
+    )
 
 
 def load_fused_pc(shard_file_pattern, sensor_id, max_dist=-1, start_frame=None, stop_frame=None, step_frame=None):
